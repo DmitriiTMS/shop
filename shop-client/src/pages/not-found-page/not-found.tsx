7a2ccc67c75c0f566c0b72efaa7f66174220912a
DmitriiTMS <dmitrii12_88@mail.ru>
2025-02-09 17:51:26 +0300

    оформил страницу not-found

--- shop-client/src/pages/not-found-page/not-found.tsx
+++ shop-client/src/pages/not-found-page/not-found.tsx
@@ -1,3 +1,29 @@
+
+import { Link } from 'react-router-dom';
+import ponchik from '../../assets/images/not-found/ponchik.png';
+
+import styles from './not-found.module.css';
+
 export const NotFoundPage = () => {
-    return "NotFoundPage"
+    return (
+        <section>
+            <div className="container">
+                <div className={styles.notFoundBlock}>
+                    <div>
+                        <h3 className={styles.notFoundBlockTitle}>Ошибка</h3>
+                        <div className={styles.notFoundBlockNumbers}>
+                            <p className={styles.notFoundBlockNumber}>4</p>
+                            <img src={ponchik} alt="ponchik" />
+                            <p className={styles.notFoundBlockNumber}>4</p>
+                        </div>
+                        <p className={styles.notFoundBlockText}>Ой! Кажется что-то пошло не так. Страница, которую вы запрашиваете, не существует. 
+                            Возможно она устарела, была удалена, или был введен неверный адрес в адресной строке.</p>
+                        <Link className={styles.notFoundBlockLink} to="/">Вернуться на главную</Link>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+    )
 }
